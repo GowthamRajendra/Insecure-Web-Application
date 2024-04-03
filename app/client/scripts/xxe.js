f83efc0a7ec235381cad2151fae27df79f2133c9
gowtham <gowtham.rajendra@ontariotechu.net>
2024-04-03 01:24:20 -0400

    blog page fully functional
    added feature to create blogs. fixed bugs with images

--- app/client/scripts/xxe.js
+++ app/client/scripts/xxe.js
@@ -7,12 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }).then(data => {
         // console.log(data);
         // blogs get turned into an array of objects under the key 'blog'
+        // probably because xml allows multiple elements with the same name
+        // but json does not so it puts them in an array with that name
         // each blog is now part of the array 
         let blogs = data.blogs.blog; 
         console.log(blogs);
         let blogElems = document.getElementById('blogs');
         blogs.forEach(blog => {
             let blogElem = createBlogElement(blog);
+            blogElem.appendChild(document.createElement('hr'));
             blogElems.appendChild(blogElem);
         });
     }).catch(e => {
@@ -26,25 +29,149 @@ document.addEventListener('DOMContentLoaded', function() {
         titleElem.classList.add('title', 'is-3');
         blogElem.appendChild(titleElem);
 
-        let imageElem = document.createElement('img');
-        
-        fetch('/xxe/getBlogImage?image=' + blog.image).then(response => {
-            return response.blob();
-        }).then(blob => {
-            let url = URL.createObjectURL(blob);
-            imageElem.src = url;
-        }).catch(e => {
-            console.log('Error: ' + e.message);
-        });
+        // only add the image if there is one
+        if (blog.image != '') {
+            let imageElem = document.createElement('img');
 
-        imageElem.alt = blog.title;
-        blogElem.appendChild(imageElem);
+            let extension;
+
+            // make another request to get the image
+            fetch('/xxe/getBlogImage?image=' + blog.image, {
+                method: 'GET',
+            }).then(response => {
+                // get extension of the image
+                let contentType = response.headers.get('Content-Type');
+                extension = contentType.substring(contentType.indexOf('/') + 1);
+
+                if (extension == 'svg') {
+                    return response.text();
+                } else {
+                    return response.blob();
+                }
+            }).then(data => {
+                let url;
+                if (extension == 'svg') {
+                    url = 'data:image/svg+xml,' + encodeURIComponent(data);
+                } else {
+                    url = URL.createObjectURL(data);
+                }
+                imageElem.src = url;
+            }).catch(e => {
+                console.log('Error: ' + e.message);
+            });
+
+            imageElem.alt = blog.title;
+
+            blogElem.appendChild(imageElem);
+        }
+
+        // store the author and date in a div
+        let bylineElem = document.createElement('div');
+        bylineElem.style.display = 'flex';
+        bylineElem.style.justifyContent = 'space-between';
+
+        let authorElem = document.createElement('p');
+        authorElem.appendChild(document.createTextNode('By: ' + blog.author));
+
+        let dateElem = document.createElement('p');
+        dateElem.appendChild(document.createTextNode(blog.date));
+        
+        bylineElem.appendChild(authorElem);
+        bylineElem.appendChild(dateElem);    
+        blogElem.appendChild(bylineElem);
 
         let contentElem = document.createElement('p');
         contentElem.appendChild(document.createTextNode(blog.content));
+        contentElem.style.fontSize = '1.5rem';
         blogElem.appendChild(contentElem);
 
         return blogElem;
     }
+
+    // show the file name when a file is selected
+    const fileInput = document.querySelector(".file-input");
+    fileInput.onchange = () => {
+        if (fileInput.files.length > 0) {
+            const fileName = document.querySelector(".file-name");
+            fileName.textContent = fileInput.files[0].name;
+        }
+    };
+
+    // post new blog
+    document.getElementById('blog-form').addEventListener('submit', function(event) {
+        // prevet form from submitting normally
+        event.preventDefault();
+
+        let formData = new FormData(event.target);
+
+        let title = formData.get('title');
+        let author = formData.get('name');
+        let content = formData.get('content');
+        let image = formData.get('image');
+
+        // get date and remove the day
+        let dateObj = new Date().toDateString();
+        let date = dateObj.substring(dateObj.indexOf(' ') + 1);
+        console.log(date);
+
+        if (title == '' || content == '') {
+            // show help text if fields are empty
+            let elems = document.getElementsByClassName('help-hide');
+            for (let i = 0; i < elems.length; i++) {
+                elems[i].classList.remove('is-hidden');
+            }
+
+            alert('Please add a title and content to the blog.');
+            return;
+        }
+
+        // change image name
+        let imageName = 'image-' + Date.now() + image.name.substring(image.name.lastIndexOf('.'));
+        image = new File([image], imageName, {type: image.type});
+
+        // convert blog to xml
+        let blog = '<blog>';
+        blog += '<image>' + image.name + '</image>';
+        blog += '<title>' + title + '</title>';
+        blog += '<content>' + content + '</content>';
+        blog += '<author>' + author + '</author>';
+        blog += '<date>' + date + '</date>';
+        blog += '</blog>';
+
+        fetch('/xxe/postBlog', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:  JSON.stringify({
+                "blog": blog,
+            })
+        }).then(response => {
+            return response.text();
+        }).then(data => {
+            console.log(data); // response fromserver
+        }).catch(e => {
+            console.log('Error: ' + e.message);
+        });
+
+        let newFormData = new FormData();
+        newFormData.append('image', image);
+
+        // send the image
+        fetch('/xxe/postBlogImage', {
+            method: 'POST',
+            body: newFormData
+        }).then(response => {
+            return response.text();
+        }).then(data => {
+            console.log(data); // response fromserver
+
+            // reload the page
+            window.location.reload();
+        }).catch(e => {
+            console.log('Error: ' + e.message);
+        });
+
+    });
 
 });
--- app/server/routes/xxe/xxeServer.js
+++ app/server/routes/xxe/xxeServer.js
@@ -1,12 +1,27 @@
 // change name later
 const express = require('express');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+const multer = require('multer');
 const xml2js = require('xml2js');
 const { XMLParser } = require('fast-xml-parser');
 const libxmljs = require('libxmljs2');
 const fs = require('fs');
+
 const router = express.Router();
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.join(__dirname, 'images'));
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.use(express.json());
 
 router.get('/', (req, res) => {
     // if there is no security cookie, set it to low
@@ -56,17 +71,87 @@ router.get('/getBlogImage', (req, res) => {
     // get the image name
     let image = req.query.image;
     let imagePath = path.join(__dirname, "images", image);
-    console.log(imagePath);
     // read the image file
-    fs.readFile(imagePath, (err, data) => {
+    fs.readFile(imagePath, (err, data) => { 
         if (err) {
             console.error(err);
             res.status(500).send('Internal Server Error');
             return;
         }
 
+        // get the image type
+        let type = path.extname(image);
+
+        // set the content type
+        res.setHeader('Content-Type', 'image/' + type.substring(1));
+
+        // send the image
         res.send(data);
     });
+});
+
+router.post('/postBlog', (req, res) => {
+    // get the xml blog
+    let blog = req.body.blog;
+
+    // open xml file
+    fs.readFile(path.join(__dirname, 'blogs.xml'), 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+
+        // parse the xml file
+        const xmlDoc = libxmljs.parseXmlString(data, {xinclude: true, noent : true, noblanks: true, } );
+        const root = xmlDoc.root(); // root element
+
+        // create a new blog element
+        const newBlog = libxmljs.parseXmlString(blog, {xinclude: true, noent : true, noblanks: true, });
+
+        // this is an absolutely insane way to do this 
+        // but i had to do it this way because
+        // trying to add the entity to the xml file directly was not working
+        // it is the only simple way to justify the 'developer' using xml entities
+
+        // if the author is empty, replace it with an entity
+        if (newBlog.get('author').text() == '') {
+            // create new xml with the entity and author 
+            const authorWithEntity = libxmljs.parseXmlString(
+                '<!DOCTYPE foo [<!ENTITY author "Anonymous">]><author>&author;</author>', 
+                {xinclude: true, noent : true, noblanks: true, });
+
+            // replace author node from the new blog with the author with entity node 
+            newBlog.get('author').remove();
+            newBlog.root().addChild(authorWithEntity.root());
+        }
+
+        // add the new blog to the root element
+        root.addChild(newBlog.root());
+
+        // write the new xml to the file
+        fs.writeFile(path.join(__dirname, 'blogs.xml'), xmlDoc.toString(), (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Internal Server Error');
+                return;
+            } 
+
+            res.send('Blog posted successfully');
+        });
+    });    
+});
+
+router.post('/postBlogImage', upload.single('image'), (req, res) => {
+    // get the image
+    let image = req.file;
+    console.log(image);
+    if (!image) {
+        res.status(400).send('No image found');
+        return;
+    }
+
+    res.send('Image uploaded successfully');
 });
 
 module.exports = router;
